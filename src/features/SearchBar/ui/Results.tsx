5c25d12274bfb6b71fe0d8bf816ecd238d1cf472
Brody Wingrove <88740722+BroD54@users.noreply.github.com>
2024-06-22 02:16:45 -0500

    Added info from all available dbs

--- src/features/SearchBar/ui/Results.tsx
+++ src/features/SearchBar/ui/Results.tsx
@@ -4,17 +4,25 @@ interface ResultsProps {
 
 const Results = ({ results }: ResultsProps) => {
   return (
-    <div className="absolute top-32 left-1/2 transform -translate-x-1/2 w-2/5 max-w-xl bg-white shadow-lg rounded-lg overflow-hidden z-10">
-      <ul className="max-h-72 overflow-y-auto">
-        {results.map(course => (
-          <li key={course.code} className="px-4 py-3 border-b border-gray-300 last:border-b-0 hover:bg-gray-100 transition-colors flex items-center">
-            <div className="flex-shrink-0 w-16 text-gray-600">{course.code}</div>
-            <div className="flex-grow ml-2 overflow-hidden">
-              <div className="font-semibold">{course.title}</div>
+    <div className="absolute top-32 left-1/2 transform -translate-x-1/2 w-2/5 max-w-xl bg-white rounded-xl shadow-lg shadow-gray-500 overflow-hidden">
+      <div className="max-h-48 overflow-y-auto">
+        {results.map((course, index) => (
+          <div key={index} className="flex flex-col p-4 border-b hover:bg-gray-100 cursor-pointer">
+            <div className="flex justify-between">
+              <div className="font-bold">{course.code}</div>
+              <div className="flex-1 ml-4 overflow-hidden">
+                <div className="whitespace-nowrap overflow-hidden overflow-ellipsis">{course.title}</div>
+              </div>
             </div>
-          </li>
+            <div className="text-sm text-gray-500 mt-1">
+              {course.database_name}
+              {course.database_name.includes('Fall 2024') && (
+                <span className="text-gray-400 ml-1">&#9733;</span>
+              )}
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
