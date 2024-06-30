f94f108c90098ab313387b9b078372dfa3483a39
Brody Wingrove <88740722+BroD54@users.noreply.github.com>
2024-06-30 17:55:55 -0500

    pulled important parts of FlowComponent into accessible 'hook'

--- src/features/Flow/ui/FlowComponent.tsx
+++ src/features/Flow/ui/FlowComponent.tsx
@@ -1,12 +1,6 @@
-import { useCallback, useState } from 'react';
 import ReactFlow, {
-  useNodesState,
-  useEdgesState,
-  addEdge,
   Controls,
   Background,
-  useReactFlow,
-  OnNodesDelete,
   MiniMap,
   Node
 } from 'reactflow';
@@ -14,11 +8,10 @@ import 'reactflow/dist/style.css';
 import { useSelectedCourse } from '../../../context/SelectedCourseContext';
 import CourseNode from '../../../components/nodes/CourseNode';
 import useDelayedEffect from '../../../hooks/useDelayedEffect';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import useFlow from '../../../hooks/useFlow';
 
-const connectionLineStyle = { stroke: '#2D2D2D' };
-const snapGrid = [20, 20] as [number, number];
 const nodeTypes = {
   courseNode: CourseNode,
 };
@@ -27,104 +20,33 @@ const defaultViewport = { x: 0, y: 0, zoom: 1 }; // Set initial zoom to 1
 
 const FlowComponent = () => {
   const { setSelectedCourse, addCourse } = useSelectedCourse();
-  const { getZoom } = useReactFlow();
-  const [addedCourses, setAddedCourses] = useState<string[]>([]);
-  const [deletedCourses, setDeletedCourses] = useState<Set<string>>(new Set());
-  const [selectedNodeId, setSelectedNodeId] = useState<string | null>(null);
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-
-  const AddCourse = (course: Course) => {
-    if (!addedCourses.includes(course.code)) {
-      if (deletedCourses.has(course.code)) {
-        setDeletedCourses((prev) => {
-          const newSet = new Set(prev);
-          newSet.delete(course.code);
-          return newSet;
-        });
-      }
-
-      // Get the viewport dimensions and center position
-      const centerX = document.documentElement.clientWidth / 2 - 240 / 2;
-      const centerY = document.documentElement.clientHeight / 2;
-      const zoom = getZoom();
-
-
-      const newNode = {
-        id: `${course.code}`,
-        type: 'courseNode',
-        position: { x: centerX / zoom, y: centerY / zoom },
-        data: { course },
-      };
-      setNodes((nds) => [...nds, newNode]);
-      setAddedCourses([...addedCourses, course.code]);
-
-      toast.success(`${course.code} was added`, {
-        position: 'bottom-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        progress: undefined,
-        className: 'toast-success', // >
-      });
-    } else {
-      setSelectedNodeId(course.code);
-      toast.warn(`${course.code} already exists`, {
-        position: 'bottom-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        progress: undefined,
-        className: 'toast-warn', // >
-      });
-    }
-  };
-
-  const DeleteCourse = (course: Course) => {
-    if (course) {
-      setNodes((nds) => nds.filter((node) => node.id !== course.code));
-      setDeletedCourses((prev) => new Set(prev).add(course.code));
-
-      toast.error(`${course.code} was deleted`, {
-        position: 'bottom-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        className: 'toast-error', // > 
-      });
-    }
-  };
-
-  const onNodesDelete: OnNodesDelete = (nodes) => {
-    nodes.forEach((node) => {
-      if (node.data && node.data.course) {
-        DeleteCourse(node.data.course);
-      }
-    });
-  };
-
-  const handleNodeClick = (node: Node<any, string | undefined>) => {
-    setSelectedNodeId(node.id);
-    setSelectedCourse(node.data.course)
-
-  };
+  const {
+    nodes,
+    edges,
+    snapGrid,
+    onNodesChange,
+    onEdgesChange,
+    addCourse: addCourseToFlow,
+    onNodesDelete,
+    onConnect,
+    selectedNodeId,
+    connectionLineStyle,
+    setSelectedNodeId,
+  } = useFlow();
 
   useDelayedEffect(() => {
     if (addCourse != undefined) {
-      AddCourse(addCourse);
+      addCourseToFlow(addCourse);
     }
   }, [addCourse]);
 
-  const onConnect = useCallback(
-    (params: any) =>
-      setEdges((eds) => addEdge({ ...params, style: connectionLineStyle }, eds)),
-    []
-  );
+  const handleNodeClick = (
+    _event: React.MouseEvent,
+    node: Node
+  ) => {
+    setSelectedNodeId(node.id);
+    setSelectedCourse(node.data.course);
+  };
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -133,14 +55,14 @@ const FlowComponent = () => {
           if (node.id === selectedNodeId) {
             return {
               ...node,
-              className: 'pulse'
+              className: 'pulse',
             };
           }
           return node;
         })}
         edges={edges}
         onNodesChange={onNodesChange}
-        onNodeClick={(_event, node) => handleNodeClick(node)}
+        onNodeClick={handleNodeClick}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onNodesDelete={onNodesDelete}
