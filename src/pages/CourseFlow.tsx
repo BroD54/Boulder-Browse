715660e48b4479f8a4514d86bbf8d27bebb0236b
Brody Wingrove <88740722+BroD54@users.noreply.github.com>
2024-06-30 02:51:16 -0500

    Added sidebar

--- src/pages/CourseFlow.tsx
+++ src/pages/CourseFlow.tsx
@@ -1,11 +1,13 @@
 import { SelectedCourseProvider } from '../context/SelectedCourseContext'
 import SearchBar from '../features/SearchBar/SearchBar'
 import Flow from '../features/Flow/Flow'
+import SideBar from '../features/SideBar/SideBar'
 
 const CourseFlow = () => {
     return (
         <SelectedCourseProvider>
             <Flow />
+            <SideBar />
             <SearchBar />
         </SelectedCourseProvider>
     )
