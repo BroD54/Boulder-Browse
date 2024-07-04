a705d7ac81adb9a4e21abcef3a03039991c3af88
Brody Wingrove <88740722+BroD54@users.noreply.github.com>
2024-07-04 00:07:46 -0500

    Added new controls and full layouting

---
+++ src/components/nodes/HorizontalCourseNode.tsx
@@ -0,0 +1,45 @@
+import { memo } from 'react';
+import { Handle, Position } from 'reactflow';
+import DatabaseNameFormat from '../../features/Flow/utils/DatabaseNameFormat';
+
+interface CourseNodeProps {
+  data: {
+    onChange: () => void
+    course: Course
+  }
+}
+
+const HorizontalCourseNode = memo(({ data }: CourseNodeProps) => {
+  const course = data.course
+
+  return (
+    <div className="bg-white p-2 border border-black rounded w-60 hover:bg-slate-50 hover:border-2">
+      <Handle
+        type="target"
+        position={Position.Left}
+        className="w-2 h-2"
+        isConnectable={true}
+      />
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col items-start w-16">
+          <div className="font-bold text-xs">{course.code}</div>
+          <div className="text-xs text-gray-400">
+            <DatabaseNameFormat name={course.database_name} />
+          </div>
+        </div>
+        <div className="ml-2 flex-1 text-center flex items-center">
+          <div className="whitespace-normal break-words text-sm">{course.title}</div>
+        </div>
+      </div>
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="a"
+        className="w-2 h-2"
+        isConnectable={true}
+      />
+    </div>
+  );
+});
+
+export default HorizontalCourseNode;
--- src/features/Flow/ui/FlowComponent.tsx
+++ src/features/Flow/ui/FlowComponent.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import ReactFlow, {
   useNodesState,
   useEdgesState,
@@ -7,20 +7,27 @@ import ReactFlow, {
   Background,
   OnNodesDelete,
   MiniMap,
-  Node
+  ControlButton,
+  Node,
+  Edge
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { useSelectedCourse } from '../../../context/SelectedCourseContext';
 import CourseNode from '../../../components/nodes/CourseNode';
+import HorizontalCourseNode from '../../../components/nodes/HorizontalCourseNode';
 import useDelayedEffect from '../../../hooks/useDelayedEffect';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import FlowData from "../../../data/flow/FlowData.json"
+import FlowData from "../../../data/flow/FlowData.json";
+import ELK from 'elkjs/lib/elk.bundled.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faProjectDiagram, faExchangeAlt, faArrowRight } from '@fortawesome/free-solid-svg-icons'; // Import the layout icon
 
 const connectionLineStyle = { stroke: '#2D2D2D' };
 const snapGrid = [20, 20] as [number, number];
 const nodeTypes = {
   courseNode: CourseNode,
+  horizontalCourseNode: HorizontalCourseNode
 };
 
 const defaultViewport = { x: 0, y: 0, zoom: 1 }; // Set initial zoom to 1
@@ -31,13 +38,41 @@ const FlowComponent = () => {
   const [deletedCourses, setDeletedCourses] = useState<Set<string>>(new Set());
   const [nodes, setNodes, onNodesChange] = useNodesState(FlowData.nodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(FlowData.edges);
+  const [nodeType, setNodeType] = useState('courseNode');
+  const [edgeType, setEdgeType] = useState('default');
+  const [isHorizontalLayout, setIsHorizontalLayout] = useState(true);
 
+  const toggleNodeType = () => {
+    const newType = nodeType === 'courseNode' ? 'horizontalCourseNode' : 'courseNode';
+    setNodeType(newType);
+
+    const updatedNodes = nodes.map(node => ({
+      ...node,
+      type: newType
+    }));
+  
+    setNodes(updatedNodes);
+  };  
+
+  const toggleEdgeType = () => {
+    const newType = edgeType === 'smoothstep' ? 'default' : 'smoothstep'
+    setEdgeType(newType);
+
+    const updatedEdges = edges.map(edge => ({
+      ...edge,
+      type: newType
+    }));
+  
+    setEdges(updatedEdges);
+  };
+  
   const addEdgeBetweenCourses = (sourceCode: string, targetCode: string) => {
-    const newEdge = {
+    const newEdge: Edge = {
       id: `${sourceCode}-${targetCode}`,
       source: sourceCode,
       target: targetCode,
       animated: false,
+      type: edgeType,
       style: connectionLineStyle,
     };
   
@@ -46,9 +81,9 @@ const FlowComponent = () => {
 
   useDelayedEffect(() => {
     if (edge != null) {
-        addEdgeBetweenCourses(edge[0], edge[1])
+      addEdgeBetweenCourses(edge[0], edge[1]);
     }
-  }, [edge])
+  }, [edge]);
 
   const AddCourse = (course: Course) => {
     if (!addedCourses.includes(course.code)) {
@@ -60,11 +95,11 @@ const FlowComponent = () => {
         });
       }
 
-      setSelectedCourse(null)
+      setSelectedCourse(null);
 
-      const newNode = {
+      const newNode: Node = {
         id: `${course.code}`,
-        type: 'courseNode',
+        type: nodeType,
         position: { x: Math.random() * 800, y: Math.random() * 800 },
         data: { course },
       };
@@ -80,7 +115,7 @@ const FlowComponent = () => {
         pauseOnHover: true,
         progress: undefined,
         className: `toast-success`
-      })
+      });
     } else {
       setSelectedCourse(course);
       toast.warn(`${course.code} already exists`, {
@@ -100,7 +135,7 @@ const FlowComponent = () => {
       setNodes((nds) => nds.filter((node) => node.id !== course.code));
       setDeletedCourses((prev) => new Set(prev).add(course.code));
       setAddedCourses((prev) => prev.filter((code) => code !== course.code));
-      setAddCourse(null)
+      setAddCourse(null);
 
       toast.error(`${course.code} was deleted`, {
         position: 'bottom-right',
@@ -127,18 +162,54 @@ const FlowComponent = () => {
     setSelectedCourse(node.data.course);
   };
 
-
   useDelayedEffect(() => {
     if (addCourse != undefined) {
       AddCourse(addCourse);
     }
   }, [addCourse]);
 
-  const onConnect = useCallback(
-    (params: any) =>
-      setEdges((eds) => addEdge({ ...params, style: connectionLineStyle }, eds)),
-    []
-  );
+  const handleAddEdge = (params: any) => {
+    const newEdge = {
+      ...params,
+      type: edgeType,
+      style: connectionLineStyle,
+    };
+    setEdges((eds) => addEdge(newEdge, eds));
+  };
+
+
+  const handleLayout = async () => {
+    const elk = new ELK();
+    const layoutDirection = isHorizontalLayout ? 'DOWN' : 'RIGHT';
+    setIsHorizontalLayout(!isHorizontalLayout)
+
+    const elkGraph = {
+      id: 'root',
+      layoutOptions: { 'elk.algorithm': 'layered', 'elk.direction': layoutDirection },
+      children: nodes.map(node => ({
+        id: node.id,
+        width: 240,
+        height: 60
+      })),
+      edges: edges.map(edge => ({
+        id: edge.id,
+        sources: [edge.source],
+        targets: [edge.target]
+      }))
+    };
+
+    const newGraph = await elk.layout(elkGraph);
+
+    const updatedNodes = nodes.map(node => {
+      const nodeLayout = newGraph.children?.find(n => n.id === node.id);
+      return {
+        ...node,
+        position: { x: nodeLayout?.x, y: nodeLayout?.y }
+      };
+    });
+
+    setNodes(updatedNodes as Node[]);
+  };
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -156,7 +227,7 @@ const FlowComponent = () => {
         onNodesChange={onNodesChange}
         onNodeClick={(_event, node) => handleNodeClick(node)}
         onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
+        onConnect={handleAddEdge}
         onNodesDelete={onNodesDelete}
         nodeTypes={nodeTypes}
         connectionLineStyle={connectionLineStyle}
@@ -167,7 +238,17 @@ const FlowComponent = () => {
         minZoom={0.35}
       >
         <Background />
-        <Controls />
+        <Controls>
+          <ControlButton onClick={handleLayout} title="auto layout" >
+            <FontAwesomeIcon icon={faProjectDiagram} />
+          </ControlButton>
+          <ControlButton onClick={toggleNodeType} title="flip node direction">
+            <FontAwesomeIcon icon={faExchangeAlt} />
+          </ControlButton>
+          <ControlButton onClick={toggleEdgeType} title="switch edge type">
+            <FontAwesomeIcon icon={faArrowRight} />
+          </ControlButton>
+        </Controls>
         <MiniMap />
       </ReactFlow>
       <ToastContainer />
