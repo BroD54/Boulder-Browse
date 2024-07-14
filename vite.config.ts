c7691e4d9be0b9434cf91c3459b598da8e3cd995
Brody Wingrove <88740722+BroD54@users.noreply.github.com>
2024-07-14 16:57:19 -0500

    more updates for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Boulder-Browse",
   plugins: [react()],
   build: {
     rollupOptions: {
